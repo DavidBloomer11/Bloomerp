d4b1065f9652f9d062f9e312dc1113713274ad35
davidbloomer11 <bloomer.david@outlook.com>
2024-11-25 20:30:07 +0100

    Updating tinymce to fix bug that it doesn't load correctly, and added LLM functionality for simple tinymce editor

--- bloomerp/components/llm_executor.py
+++ bloomerp/components/llm_executor.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpRequest
 from bloomerp.utils.llm import BloomerpOpenAI
 from bloomerp.models import ApplicationField
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 
 @login_required
 @route('llm_executor')
@@ -11,20 +12,23 @@ def llm_executor(request:HttpRequest) -> HttpResponse:
     '''
     Component to execute LLM queries.
     '''
-
-    query_type = request.GET.get('llm_query_type', 'sql')
+    query_type = request.GET.get('llm_query_type', None)
     query = request.GET.get('llm_query', None)
-
-    llm_query_types = ['sql', 'document_template']
-
+    llm_query_types = ['sql', 'document_template', 'tiny_mce_content']
+    
     if not query:
         return HttpResponse('No llm query provided')
 
     if query_type not in llm_query_types:
         return HttpResponse('Invalid llm query type, must be one of: ' + ', '.join(llm_query_types))
 
+    # Get the OpenAI key from the settings
+    openai_key = settings.BLOOMERP_SETTINGS.get('OPENAI_API_KEY', None)
+    if not openai_key:
+        return HttpResponse('OpenAI key not found in settings')
+
     # Init the OpenAI class
-    openai = BloomerpOpenAI()
+    openai = BloomerpOpenAI(openai_key)
 
     # Check if the key is valid
     if not openai.is_valid_key():
@@ -37,8 +41,7 @@ def llm_executor(request:HttpRequest) -> HttpResponse:
         return HttpResponse(sql_query)
     
     elif query_type == 'document_template':
-        
         return HttpResponse('Document template AI is not implemented yet')
-
-    context = {}
-    return render(request, 'components/llm_executor.html', context)
+    elif query_type == 'tiny_mce_content':
+        tiny_mce_content = openai.create_tiny_mce_content(query)
+        return HttpResponse(tiny_mce_content)
