ce4a4bab3b9e1b43da7375bc50e18282d1bb83c4
davidbloomer11 <bloomer.david@outlook.com>
2025-01-13 20:43:55 +0100

    Bump version to 0.3.2-dev2 in setup.py for development release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="Bloomerp",
-    version="0.3.2-dev",
+    version="0.3.2-dev2",
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",
