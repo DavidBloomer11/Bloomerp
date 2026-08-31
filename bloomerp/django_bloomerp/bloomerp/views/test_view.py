f56eaccc548ce3116e02586c195193682c4856b4
David Bloomer <bloomer.david@outlook.com>
2026-08-31 21:20:54 +0200

    Add simple Markdown template and update TestView to use MarkdownView

--- bloomerp/django_bloomerp/bloomerp/views/test_view.py
+++ bloomerp/django_bloomerp/bloomerp/views/test_view.py
@@ -1,41 +1,15 @@
-from bloomerp.models import FieldLayout, LayoutItem, LayoutRow
 from bloomerp.router import router
-from bloomerp.views.base import BaseBloomerpView
-from bloomerp.views.mixins.layout_form_mixin import LayoutFormMixin
-from bloomerp.views.mixins.layout_mixin import LayoutMixin
-from django.views.generic import TemplateView
-from django import forms
-
-class RandomForm(forms.Form):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-
-
+from bloomerp.views.generic.markdown import MarkdownView
 
 @router.route(
     "test-view"
 )
-class TestView(LayoutFormMixin, BaseBloomerpView, TemplateView):
-    template_name = "mixins/bloomerp_layout_form_mixin.html"
+class TestView(MarkdownView):
+    markdown_file = "test.md"
     
-    can_change = False
-    form_class = RandomForm
     
-    layout = FieldLayout(
-        rows=[LayoutRow(
-            columns=2,
-            items=[
-                LayoutItem(
-                    id="last_name",
-                    colspan=1,
-                ),
-                LayoutItem(
-                    id="first_name",
-                    colspan=1,
-                )
-            ]
-        )]
-    )
+    
+    
     
     
     
